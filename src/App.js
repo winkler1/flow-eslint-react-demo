18d4549fa3cc002c08c3d6de3c34c433322e226c
Jeff Winkler <winkler1@gmail.com>
2016-02-19 09:50:27 -0500

    createClass Greeter, with undocumented prop.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,17 @@
-import React, { Component } from 'react';
-import { NICE, SUPER_NICE } from './colors';
+/* @flow */
+import React, {Component,PropTypes} from 'react';
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { counter: 0 };
-    this.interval = setInterval(() => this.tick(), 1000);
-  }
-
-  tick() {
-    this.setState({
-      counter: this.state.counter + this.props.increment
-    });
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
+var Greeter = React.createClass({
   render() {
-    return (
-      <h1 style={{ color: this.props.color }}>
-        Counter ({this.props.increment}): {this.state.counter}
-      </h1>
-    );
+    return <div>Hello {this.props.name}</div>
   }
-}
+});
 
 export class App extends Component {
-  render() {
+  render():React.Element {
     return (
       <div>
-        <Counter increment={1} color={NICE} />
-        <Counter increment={5} color={SUPER_NICE} />
+        <Greeter/>
       </div>
     );
   }
